5d04e23cf61c642958a58a88a56782a8bc352868
Nathan Shea <github.4tivd@simplelogin.com>
2023-12-19 09:27:28 +1030

    removed mdx due to failed build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,27 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import NetlifyCMS from 'astro-netlify-cms'
+import { defineConfig } from 'astro/config';
+// import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import NetlifyCMS from 'astro-netlify-cms';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://UBU-Team.github.io',
   compressHTML: true,
-  integrations: [mdx(), tailwind({
-    applyBaseStyles: false,
+  integrations: [
+    // mdx(), 
+    tailwind({
+    applyBaseStyles: false
   }), compress(), NetlifyCMS({
     config: {
       backend: {
         name: 'git-gateway',
-        branch: 'main',
+        branch: 'main'
       },
       collections: [
         //content collections
-      ],
-    },
-  }),
-],
+      ]
+    }
+  })]
 });
